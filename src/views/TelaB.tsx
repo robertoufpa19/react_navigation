98a12aa862bd0a22c692a1002d2e3428e07bcc8e
Roberto Rodrigues <robertoufpa19@gmail.com>
2024-01-23 22:26:49 -0300

    adicionando as bibliotecas do react navigation

---
+++ src/views/TelaB.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import TextoCentral from '../components/TextoCentral'
+
+export default props =>(
+
+    <TextoCentral corFundo='#e67890'>
+        Tela B
+    </TextoCentral>
+)
---
+++ src/views/TelaC.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import TextoCentral from '../components/TextoCentral'
+
+export default props =>(
+
+    <TextoCentral corFundo='#e57812'>
+        Tela C
+    </TextoCentral>
+)
